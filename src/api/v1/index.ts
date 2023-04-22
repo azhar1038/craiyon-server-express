2764e4f6789236f8f6a0a8b7287a5332e7e2e1f3
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2023-04-22 18:25:10 +0530

    Remove an unnecessary file

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,10 +1,16 @@
 import { Application, Request, Response } from 'express';
 import { registerMiddleware } from '../middleware';
-import { registerApiRoutes } from './components/components';
+import { UserRoutes } from './components/user/user-routes';
+import { AuthRoutes } from './components/auth/auth-routes';
 
 export function initRestRoutesV1(app: Application): void {
   const prefix = '/api/v1';
   registerMiddleware(app);
   app.get(prefix, (req: Request, res: Response) => res.send('v1'));
-  registerApiRoutes(app, prefix);
+
+  const userRoutes: UserRoutes = new UserRoutes();
+  app.use(`${prefix}/${userRoutes.name}`, userRoutes.router);
+
+  const authRoutes: AuthRoutes = new AuthRoutes();
+  app.use(`${prefix}/${authRoutes.name}`, authRoutes.router);
 }
